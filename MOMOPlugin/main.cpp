6c9314d779148a54365ce5b65b73e9e32c295f2a
Arvind <Arvind@DESKTOP-1H3RAB5>
2020-11-15 16:58:56 +0530

    First Commit

---
+++ MOMOPlugin/main.cpp
@@ -0,0 +1,23 @@
+#include <OpenNI.h>
+
+using namespace openni;
+
+int main()
+{
+	Status rc = OpenNI::initialize();
+	if (rc != STATUS_OK)
+	{
+		printf("Initialize failed\n%s\n", OpenNI::getExtendedError());
+		return 1;
+	}
+
+	Device device;
+	rc = device.open(ANY_DEVICE);
+	if (rc != STATUS_OK)
+	{
+		printf("Couldn't open device\n%s\n", OpenNI::getExtendedError());
+		return 2;
+	}
+
+	return 0;
+}
