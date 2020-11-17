39037fef8b34bb07afaf341190c298ba7004fe83
Arvind <Arvind@DESKTOP-1H3RAB5>
2020-11-17 20:41:11 +0530

    Segmentation

--- MOMOPlugin/main.cpp
+++ MOMOPlugin/main.cpp
@@ -8,6 +8,10 @@
 #include <pcl/common/time.h>
 #include <pcl/segmentation/sac_segmentation.h>
 #include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/common/transforms.h>
 
 
 using namespace openni;
@@ -20,46 +24,35 @@ std::shared_ptr<pcl::io::openni2::OpenNI2Device> pclDevice;
 std::shared_ptr<pcl::io::OpenNI2Grabber> openni2Grabber;
 pcl::SACSegmentation<pcl::PointXYZ> seg;
 pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
-pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
+pcl::PointIndices::Ptr inlierIndices(new pcl::PointIndices);
 boost::mutex mutex;
-pcl::PointCloud<pcl::PointXYZRGBA>::ConstPtr buffer;
+
+pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloudBuffer;
+pcl::PointCloud<pcl::PointXYZ>::ConstPtr rawCloud;
+pcl::PointCloud<pcl::PointXYZ>::Ptr filteredCloud(new pcl::PointCloud<pcl::PointXYZ>);
+pcl::PointCloud<pcl::PointXYZ>::Ptr segmentedCloud(new pcl::PointCloud<pcl::PointXYZ>);
+pcl::PointCloud<pcl::PointXYZ>::Ptr wallRemovedCloud(new pcl::PointCloud<pcl::PointXYZ>);
+pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> segmentedCloudColor(segmentedCloud, 255, 0, 0);
+pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> wallRemovedCloudColor(wallRemovedCloud, 255, 255, 255);
+
+pcl::VoxelGrid<pcl::PointXYZ> voxelFilter;
+pcl::ExtractIndices<pcl::PointXYZ> pointsExtractor;
+
+
 HandTracker handTracker;
 bool shouldExit = false;
 int framecounter = 0;
 
-pcl::visualization::PCLVisualizer::Ptr viewer(new pcl::visualization::PCLVisualizer("3D Viewer"));
+pcl::visualization::PCLVisualizer pclVisualizer("PCL Visualizer");
+//pcl::visualization::CloudViewer cloudViewer("Cloud Viewer");
 
-void cloud_cb_(const pcl::PointCloud<pcl::PointXYZRGBA>::ConstPtr &cloud)
+void cloud_cb_(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr &cloud)
 {
-	/*viewer->addPointCloud(cloud);*/
-	
-	//seg.setInputCloud(cloud);
-	//seg.segment(*inliers, *coefficients);
-
-	//static unsigned count = 0;
-	//static double last = pcl::getTime();
-	//if (++count == 30)
-	//{
-	//	double now = pcl::getTime();
-	//	std::cout << "distance of center pixel :" << cloud->points[(cloud->width >> 1) * (cloud->height + 1)].z << " mm. Average framerate: " << double(count) / double(now - last) << " Hz" << std::endl;
-	//	count = 0;
-	//	last = now;
-	//}
-
 	boost::mutex::scoped_lock lock(mutex);
-	buffer = cloud->makeShared();
-
-	//if (!viewer->updatePointCloud(cloud, "cloud"))
-	//{
-	//	viewer->addPointCloud(cloud, "cloud");
-	//}
+	cloudBuffer = cloud->makeShared();
 
 	framecounter++;
-	std::cout << "Got Frame: "<<framecounter<<"\n";
-
-	//if (!viewer->updatePointCloud(cloud, "Cloud")) {
-	//	viewer->addPointCloud(cloud, "Cloud");
-	//}
+	//std::cout << "Got Frame: "<<framecounter<<"\n";
 }
 
 int Setup()
@@ -75,7 +68,6 @@ int Setup()
 	}
 
 	opennistatus = openniDevice->open(ANY_DEVICE);
-	//openniDevice->setImageRegistrationMode(ImageRegistrationMode::IMAGE_REGISTRATION_DEPTH_TO_COLOR);
 
 	if (opennistatus != openni::Status::STATUS_OK)
 	{
@@ -84,37 +76,20 @@ int Setup()
 	}	
 
 	const std::string device_id = openniDevice->getDeviceInfo().getUri();
-
-	//pcl::io::openni2::OpenNI2Device::Ptr testDevice = OpenNI2DeviceManager::getInstance()->getDevice(device_id);
-
-	//nite::Status nitestatus = NiTE::initialize();
-
-	////nitestatus = handTracker.create(&openniDevice);
-	//
-	//handTracker.startGestureDetection(GestureType::GESTURE_WAVE);
-
-	//pclDevice = std::make_shared<pcl::io::openni2::OpenNI2Device>(openniDevice);
-
 	openni2Grabber = std::make_shared<pcl::io::OpenNI2Grabber>(device_id);
 
 	// make callback function from member function
-	boost::function<void(const pcl::PointCloud<pcl::PointXYZRGBA>::ConstPtr&)> f = boost::bind(cloud_cb_, _1);
+	boost::function<void(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr&)> f = boost::bind(cloud_cb_, _1);
 	
-	//seg.setOptimizeCoefficients(true);
-	//// Mandatory
-	//seg.setModelType(pcl::SACMODEL_PLANE);
-	//seg.setMethodType(pcl::SAC_RANSAC);
-	//seg.setDistanceThreshold(0.01);
+	seg.setOptimizeCoefficients(true);
+	seg.setModelType(pcl::SACMODEL_PLANE);
+	seg.setMethodType(pcl::SAC_RANSAC);
+	seg.setDistanceThreshold(0.1);
 
-	//pcl::PointCloud<pcl::PointXYZRGB>::Ptr source_cloud(new pcl::PointCloud<pcl::PointXYZRGB>());
-	//pcl::io::loadPCDFile("test.pcd", *source_cloud);
-
-	viewer->setBackgroundColor(0.7, 0.05, 0.05, 0);
-	//viewer->addPointCloud(source_cloud);
-	//viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 2, "sample cloud");
-	viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 2);
-	viewer->addCoordinateSystem(1.0);
-	//viewer->initCameraParameters();
+	pclVisualizer.setBackgroundColor(0.02, 0.02, 0.02);
+	//pclVisualizer.setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_COLOR, 1.0, 0.0, 0.5);
+	pclVisualizer.setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "Cloud");
+	//pclVisualizer.addCoordinateSystem(1.0);
 
 	openni2Grabber->registerCallback(f);
 	openni2Grabber->start();
@@ -150,26 +125,77 @@ int main()
 {
 	Setup();
 
-	while (!viewer->wasStopped()) 
-	{   // Display the visualizer until the 'q' key is pressed
-		viewer->spinOnce();
+	//while (!cloudViewer.wasStopped())
+	//{
+	//	pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloud;
 
-		std::cout << "Hello" << "\n";
+	//	if (mutex.try_lock()) 
+	//	{
+	//		buffer.swap(cloud);
+	//		mutex.unlock();
+	//	}
 
-		//boost::this_thread::sleep(boost::posix_time::seconds(1));
+	//	if (cloud)
+	//	{
+	//		//seg.setInputCloud(cloud);
 
-		pcl::PointCloud<pcl::PointXYZRGBA>::ConstPtr cloud;
+	////		pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
+	////		pcl::PointIndices inlierIndices;
+	////		seg.segment(inlierIndices, *coefficients);
 
-		if (mutex.try_lock()) {
-			buffer.swap(cloud);
+	//		cloudViewer.showCloud(cloud);
+	//	}
+	//}
+
+	while (!pclVisualizer.wasStopped()) 
+	{  
+		if (mutex.try_lock()) 
+		{
+			cloudBuffer.swap(rawCloud);
 			mutex.unlock();
 		}
 
-		if (cloud) {
-			if (!viewer->updatePointCloud(cloud, "Cloud")) {
-				viewer->addPointCloud(cloud, "Cloud");
+		if (rawCloud) 
+		{
+			voxelFilter.setInputCloud(rawCloud);
+			voxelFilter.setLeafSize(0.02f, 0.02f, 0.02f);
+			voxelFilter.filter(*filteredCloud);
+
+			seg.setInputCloud(filteredCloud);
+			seg.segment(*inlierIndices, *coefficients);
+			std::cout << inlierIndices->indices.size() << "\n";
+
+			pointsExtractor.setInputCloud(filteredCloud);
+			pointsExtractor.setIndices(inlierIndices);
+			pointsExtractor.setNegative(false);
+			pointsExtractor.filter(*segmentedCloud);
+			pointsExtractor.setNegative(true);
+			pointsExtractor.filter(*wallRemovedCloud);
+
+			Eigen::Affine3f transform = Eigen::Affine3f::Identity();
+			transform.rotate(Eigen::AngleAxisf(M_PI /*180°*/, Eigen::Vector3f::UnitX()));
+
+			pcl::PointCloud<pcl::PointXYZ>::Ptr rotatedCloud(new pcl::PointCloud<pcl::PointXYZ>);
+			/*pcl::transformPointCloud(*wallRemovedCloud, *rotatedCloud, transform);*/
+
+			if (!pclVisualizer.updatePointCloud(wallRemovedCloud, wallRemovedCloudColor, "NoWallCloud"))
+			{
+				pclVisualizer.addPointCloud(wallRemovedCloud, wallRemovedCloudColor, "NoWallCloud");
 			}
+		
+			if (!pclVisualizer.updatePointCloud(segmentedCloud, segmentedCloudColor, "Segmented"))
+			{
+				pclVisualizer.addPointCloud(segmentedCloud, segmentedCloudColor, "Segmented");
+			}
+
+			//pclVisualizer.resetCameraViewpoint("NoWallCloud");
+
+			pclVisualizer.spinOnce();
+
+			//boost::this_thread::sleep(boost::posix_time::milliseconds(5));
 		}
+
+
 	}
 
 	/*while (!shouldExit)
